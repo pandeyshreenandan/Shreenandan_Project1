8effbff006cc712510e32912b09f44a6014ff2b9
Shreenandan Pandey <pandeyshreenandn@gmail.com>
2026-01-20 21:43:51 +0530

    upgraded portfolio website

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Welcome Shreenandan! Your portfolio is running successfully ✅");
+// Auto year
+document.getElementById("year").innerText = new Date().getFullYear();
+
+// Date for "Updated"
+const options = { year: "numeric", month: "short", day: "numeric" };
+document.getElementById("todayDate").innerText =
+  new Date().toLocaleDateString("en-IN", options);
